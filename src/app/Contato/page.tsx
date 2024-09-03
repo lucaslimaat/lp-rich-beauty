e125d04e1fe568c186dc13b077354635718e4c51
Lucas Lima Torquato <lucaslimatorquato@hotmail.com>
2024-09-02 22:02:33 -0300

    Add form in contact page

--- src/app/Contato/page.tsx
+++ src/app/Contato/page.tsx
@@ -1,5 +1,4 @@
 import SectionContact from "@/components/ContactPage/Section";
-import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
--- src/components/ContactPage/Section/index.tsx
+++ src/components/ContactPage/Section/index.tsx
@@ -12,7 +12,7 @@ export default function SectionContact() {
           <p className=' text-lg md:text-lg font-medium text-center text-gray-300'>Any question or remarks? Just write us a message!</p>
         </Balancer>
       </div>
-      <div className=' w-full h-auto bg-white flex flex-row items-start justify-center p-5'>
+      <div className=' w-full h-auto bg-white flex flex-wrap items-start justify-center p-5 gap-12'>
         <div className=' h-full w-full max-w-[491px] flex flex-col items-center justify-center md:items-start md:Sjustify-start bg-green rounded-[10px] gap-9 md:gap-28 px-[40px] py-[40px]'>
           <div className=' flex flex-col gap-[6px]'>
             <h1 className='text-white font-semibold text-xl md:text-[28px] text-center'>Informações de Contato</h1>
@@ -38,7 +38,107 @@ export default function SectionContact() {
             <FaLinkedin size={30} color='white'  />
           </div>
         </div>
+        
+        <div className='flex flex-wrap items-center justify-center mt-14'>
+  <form className='w-full max-w-lg'>
+    <div className='flex flex-wrap -mx-3 mb-4'>
+      <div className='w-full md:w-1/2 px-3 mb-4 md:mb-0'>
+        <div className="relative">
+          <input
+            className='appearance-none border-b-2 border-gray-400 w-full pt-4 px-1 text-gray-700 leading-tight focus:outline-none focus:border-black transition-colors duration-1000 peer'
+            id='firstName'
+            type='text'
+          />
+          <label
+            className='absolute left-1 top-0 text-gray-300 text-xs font-medium mb-2 transition-colors duration-1000 peer-focus:text-black'
+            htmlFor='firstName'
+          >
+            Primeiro Nome
+          </label>
+        </div>
       </div>
+      <div className='w-full md:w-1/2 px-3'>
+        <div className="relative">
+          <input
+            className='appearance-none border-b-2 border-gray-400 w-full py-2 px-1 text-gray-700 leading-tight focus:outline-none focus:border-black transition-colors duration-1000 peer'
+            id='lastName'
+            type='text'
+          />
+          <label
+            className='absolute left-1 top-0 text-gray-300 text-xs font-medium mb-2 transition-colors duration-1000 peer-focus:text-black'
+            htmlFor='lastName'
+          >
+            Sobrenome
+          </label>
+        </div>
+      </div>
+    </div>
+    <div className='flex flex-wrap -mx-3 mb-4'>
+      <div className='w-full md:w-1/2 px-3 mb-4 md:mb-0'>
+        <div className="relative">
+          <input
+            className='appearance-none border-b-2 border-gray-400 w-full py-2 px-1 text-gray-700 leading-tight focus:outline-none focus:border-black transition-colors duration-1000 peer'
+            id='email'
+            type='email'
+          />
+          <label
+            className='absolute left-1 top-0 text-gray-300 text-xs font-medium mb-2 transition-colors duration-1000 peer-focus:text-black'
+            htmlFor='email'
+          >
+            Email
+          </label>
+        </div>
+      </div>
+      <div className='w-full md:w-1/2 px-3'>
+        <div className="relative">
+          <input
+            className='appearance-none border-b-2 border-gray-400 w-full py-2 px-1 text-gray-700 leading-tight focus:outline-none focus:border-black transition-colors duration-1000 peer'
+            id='phone'
+            type='text'
+          />
+          <label
+            className='absolute left-1 top-0 text-gray-300 text-xs font-medium mb-2 transition-colors duration-1000 peer-focus:text-black'
+            htmlFor='phone'
+          >
+            Telefone
+          </label>
+        </div>
+      </div>
+    </div>
+    <div className='flex flex-wrap -mx-3 mb-4'>
+      <div className='w-full px-3'>
+        <div className="relative">
+          <textarea
+            className='appearance-none border-b-2 border-gray-400 w-full py-2 px-1 text-gray-700 leading-tight focus:outline-none focus:border-black transition-colors duration-1000 peer'
+            id='message'
+            rows='4'
+          ></textarea>
+          <label
+            className='absolute left-1 top-0 text-gray-300 text-xs font-medium mb-2 transition-colors duration-1000 peer-focus:text-black'
+            htmlFor='message'
+          >
+            Mensagem
+          </label>
+        </div>
+      </div>
+    </div>
+    <div className='flex items-center justify-center'>
+      <button
+        className='bg-black hover:bg-black text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
+        type='button'
+      >
+        Enviar
+      </button>
+    </div>
+  </form>
+</div>
+
+
+        
+</div>
+
+
+
     </div>
   )
 }
